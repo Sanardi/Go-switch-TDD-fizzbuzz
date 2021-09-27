3210f070f195e0cdfb70fb92a927216a7cc7e340
Sanardi <rubypromise@gmx.com>
2021-09-27 11:05:12 +0100

    v2 with waitgroups

--- v2/fizzbuzz.go
+++ v2/fizzbuzz.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
@@ -11,18 +12,29 @@ import (
 //When the int is divisible by 5 it prints "Buzz"
 //when the int is divisible by 3 and 5 it prints "FizzBuzz"
 func FizzBuzz(size int) []string {
+
 	var output []string
+
+	var wg sync.WaitGroup
+
+	// adding 200 goroutines to the WaitGroup
+	wg.Add(size + 1)
+
 	for i := 1; i < size+1; i++ {
 		ne := ""
 		switch {
 		case i%3 == 0 && i%5 == 0:
 			ne = "FizzBuzz"
+			wg.Done()
 		case i%3 == 0:
 			ne = "Fizz"
+			wg.Done()
 		case i%5 == 0:
 			ne = "Buzz"
+			wg.Done()
 		default:
 			ne = fmt.Sprint(i)
+			wg.Done()
 		}
 		output = append(output, ne)
 	}
